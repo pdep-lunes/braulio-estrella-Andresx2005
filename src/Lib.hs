c3d887be5faaf913d3bb980cde52e77bf304115c
Andresx2005 <andresvoet10@gmail.com>
2025-05-04 18:33:57 -0300

    Agrego reportes y personajes

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,7 @@ module Lib () where
 
 import Text.Show.Functions ()
 
-type Poder = Personaje->Personaje
+type Poder = Personaje->Personaje->Personaje
 
 data Personaje = UnPersonaje{
   nombre :: String,
@@ -12,7 +12,7 @@ data Personaje = UnPersonaje{
   cantidadVida :: Int,
   radioGranada :: Int,
   tipoTuercas :: String
-}
+} deriving(Show)
 
 esVidaNegativa :: Int -> Bool
 esVidaNegativa vida = vida < 0
@@ -23,20 +23,38 @@ ponerACeroVidaNegativa vida
   | otherwise = vida 
 
 bolaEspinosa :: Poder
-bolaEspinosa atacado = atacado{cantidadVida = ponerACeroVidaNegativa (cantidadVida atacado - 1000)}
+bolaEspinosa atacante atacado = atacado{cantidadVida = ponerACeroVidaNegativa (cantidadVida atacado - 1000)}
 
-lluviaDeTuercas :: Personaje->Poder
+lluviaDeTuercas :: Poder
 lluviaDeTuercas  atacante afectado
   |tipoTuercas atacante=="Sanadoras" = afectado{cantidadVida = cantidadVida afectado + 800}
   |tipoTuercas atacante=="Daninas" = afectado{cantidadVida = cantidadVida afectado `div` 2}
   |otherwise = afectado
 
-granadaDeEspinas :: Personaje->Poder
+granadaDeEspinas :: Poder
 granadaDeEspinas atacante atacado
-  |radioGranada atacante > 3 && atacado vida < 800 = atacado{nombre = nombre atacado ++ " Espina estuvo aqui", superPoderActivo = False, cantidadVida = 0}
+  |radioGranada atacante > 3 && cantidadVida atacado < 800 = atacado{nombre = nombre atacado ++ " Espina estuvo aqui", superPoderActivo = False, cantidadVida = 0}
   |radioGranada atacante > 3 = atacado{nombre = nombre atacado ++ " Espina estuvo aqui"}
-  |atacado vida < 800 = atacado{superPoderActivo = False, cantidadVida = 0}
-  |otherwise = bolaEspinosa atacado
+  |cantidadVida atacado < 800 = atacado{superPoderActivo = False, cantidadVida = 0}
+  |otherwise = bolaEspinosa atacante atacado
 
 torretaCurativa :: Poder
-torretaCurativa ayudado = ayudado{superPoderActivo = True, cantidadVida = cantidadVida atacado * 2}
+torretaCurativa atacante ayudado = ayudado{superPoderActivo = True, cantidadVida = cantidadVida ayudado * 2}
+
+atacarPoderEspecial :: Personaje -> Personaje -> Personaje
+atacarPoderEspecial atacante atacado = (poderBasico atacante) atacante ((superPoder atacante) atacante atacado)
+
+espina :: Personaje
+espina = UnPersonaje{nombre="Espina", poderBasico=bolaEspinosa, superPoder=granadaDeEspinas, superPoderActivo=True, cantidadVida=4800, radioGranada=5, tipoTuercas=""}
+
+pamela :: Personaje
+pamela = UnPersonaje{nombre="Pamela", poderBasico=lluviaDeTuercas, superPoder=torretaCurativa,superPoderActivo=False, cantidadVida=9600, radioGranada=0, tipoTuercas="Sanadoras"}
+
+personajes :: [Personaje]
+personajes = [espina, pamela]
+
+enLasUltimas :: Personaje -> Bool
+enLasUltimas personaje = cantidadVida personaje < 800
+
+quienesEnLasUltimas :: [Personaje]
+quienesEnLasUltimas = filter enLasUltimas personajes
